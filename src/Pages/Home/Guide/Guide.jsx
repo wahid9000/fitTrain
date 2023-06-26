c55b4914f926dadc2a8770d3c778f4ed603ef3ae
Wahid Ahmed Chowdhury <wahidchowdhury9000@gmail.com>
2023-06-26 17:58:41 +0600

    guide section done

--- src/Pages/Home/Guide/Guide.jsx
+++ src/Pages/Home/Guide/Guide.jsx
@@ -2,7 +2,37 @@
 const Guide = () => {
     return (
         <div>
-            THis is guide
+            <div className="flex p-5 justify-center gap-10 mx-auto">
+                <div className="w-7/12">
+                    <h2 className="text-orange-200 text-3xl font-bold mb-4">Fitness Basics</h2>
+                    <p className="text-justify text-white">To have a fit body, basics of fitness must have to be known. Explaining the importance of regular exercise for overall health and wellness. Discuss the benefits of different types of workouts, such as cardiovascular exercises, strength training, and flexibility exercises.Discuss the benefits of different types of workouts, such as cardiovascular exercises, strength training, and flexibility exercises and flexibility exercises.Discuss the benefits of different types of workouts, such as cardiovascular exercises, strength training, and flexibility exercises.</p>
+                </div>
+                <div>
+                    <img src="https://i.ibb.co/vz5sR28/concept-power-strength-healthy-lifestyle-sport-powerful-attractive-muscular-man-gym.jpg" className="rounded-lg" width={550} alt="" />
+                </div>
+            </div>
+
+
+            <div className="flex p-5 justify-center gap-12 mx-auto mt-10">
+                <div>
+                    <img src="https://i.ibb.co/DkHcDrb/athletic-man-woman-with-dumbbells.jpg" className="rounded-lg" width={550} alt="" />
+                </div>
+                <div className="w-7/12">
+                    <h2 className="text-orange-200 text-3xl font-bold mb-4">Goal Setting</h2>
+                    <p className="text-justify text-white">This section provides an introduction to fitness fundamentals. Explaining the importance of regular exercise for overall health and wellness. Discuss the benefits of different types of workouts, such as cardiovascular exercises, strength training, and flexibility exercises.Discuss the benefits of different types of workouts, such as cardiovascular exercises, strength training, and flexibility exercises and flexibility exercises.Discuss the benefits of different types of workouts, such as cardiovascular exercises, strength training, and flexibility exercises.</p>
+                </div>
+            </div>
+
+            <div className="flex p-5 justify-center gap-12 mx-auto mt-10">
+                <div className="w-7/12">
+                    <h2 className="text-orange-200 text-3xl font-bold mb-4">Nutrition and Meal Planning</h2>
+                    <p className="text-justify text-white">Role of nutrition in supporting fitness goals. Provide guidance on developing a balanced and nutritious meal plan. Explain the importance of macronutrients (carbohydrates, proteins, and fats) and micronutrients (vitamins and minerals) in fueling the body for workouts and promoting recovery. Offer tips for meal prepping, portion control, and choosing whole, nutrient-dense foods.role of nutrition in supporting fitness goals. Provide guidance on developing a balanced and nutritious meal plan. Explain the importance of macronutrients (carbohydrates, proteins, and fats) and micronutrients (vitamins and minerals) in fueling the body for workouts and promoting recovery. Offer tips for meal.</p>
+                </div>
+                <div>
+                    <img src="https://i.ibb.co/t2CWRCr/fitness-woman-preparing-detox-juice.jpg" className="rounded-lg" width={550} alt="" />
+                </div>
+            </div>
+
         </div>
     );
 };
